6072ad586c7e3efa88badc573a4baa02229e970f
Quetellen Johnson <quetellen.bertazo@gmail.com>
2023-11-04 11:10:00 -0700

    update, delete, search

--- src/main/java/com/simplilearn/spring/controller/UserController.java
+++ src/main/java/com/simplilearn/spring/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.simplilearn.spring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.simplilearn.spring.bean.User;
 import com.simplilearn.spring.service.UserService;
+
 
 @Controller
 public class UserController {
@@ -44,5 +51,85 @@ public class UserController {
         logger.debug("Showing Create...");
 
         return "create";
+    }
+
+    @PostMapping("/create")
+    String createUser(User user, BindingResult result) {
+
+        logger.debug("Creating User... {}",user);
+
+        this.validateUsername(user, result);
+
+        if (result.hasErrors()) {
+            return "create";
+        }
+
+        this.userService.createUser(user);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/update/{id}")
+    ModelAndView showUpdate(@PathVariable int id) {
+
+        logger.debug("Showing Update... UserID: {}", id);
+
+        User user = this.userService.findUser(id);
+
+        return new ModelAndView("update","user",user);
+    }
+
+    @PostMapping("/update")
+    String updateUser(User user, BindingResult result) {
+
+        logger.debug("Updating User... {}",user);
+
+        this.validateUsername(user, result);
+
+        if (result.hasErrors()) {
+            return "update";
+        }
+
+        this.userService.updateUser(user);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    String deleteUser(@PathVariable int id) {
+
+        logger.debug("Deleting User... UserID: {}", id);
+
+        this.userService.deleteUser(id);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/search")
+    String showSearch() {
+
+        logger.debug("Showing Search...");
+
+        return "search";
+    }
+
+    @PostMapping("/search")
+    ModelAndView searchUser(@RequestParam int id) {
+    	
+    	logger.debug("Searching User... UserID: {}", id);
+    	
+    	User user = this.userService.findUser(id);
+
+    	return new ModelAndView("table","user",user);
+    }
+    
+    void validateUsername(User user, BindingResult result) {
+
+//        if ( this.userService.findUser(user) != null) {
+        if ( Optional.ofNullable(this.userService.findUser(user)).isPresent() ) {
+
+            result.addError(new FieldError("user","username", user.getUsername(),
+                                              false,null, null,"* Username already taken."));
+        }
     }
 }
--- src/main/java/com/simplilearn/spring/service/UserService.java
+++ src/main/java/com/simplilearn/spring/service/UserService.java
@@ -12,15 +12,46 @@ import com.simplilearn.spring.dao.UserDao;
 @Service
 public class UserService {
 
-	@Autowired
-	UserDao userDao;
-	
-	public List<User> list() {
-		
-		/*
-		 * Add some Business Logic to process the list.
-		 */
-		
-		return this.userDao.list();
-	}
+    @Autowired
+    UserDao userDao;
+
+    public List<User> list() {
+
+        /*
+         * Add some Business Logic to process the list.
+         */
+
+        return this.userDao.list();
+    }
+
+    public void createUser(User user) {
+
+        this.validateUser(user);
+        this.userDao.createUser(user);
+    }
+
+    public User findUser(User user) {
+        return this.userDao.findUser(user);
+    }
+
+    public User findUser(int idUser) {
+    	return this.userDao.findUser(idUser);
+    }
+
+    public void updateUser(User user) {
+
+        this.validateUser(user);
+        this.userDao.updateUser(user);
+    }
+
+    public void deleteUser(int idUser) {
+    	this.userDao.deleteUser(idUser);
+    }
+    
+    private void validateUser(User user) {
+        if (user.getFirstName().isEmpty() || user.getLastName().isEmpty() || user.getUsername().isEmpty()) {
+            throw new RuntimeException("Invalid User Data: " + user);
+        }
+
+    }
 }
